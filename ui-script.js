f190c988b28fb74c26f811afaf6add96c9c4d794
Jaikirat Sandhu <jaikirat.sandhu@sprinklr.com>
2020-09-03 19:20:55 +0530

    Refactor Code into separate files

--- ui-script.js
+++ ui-script.js
@@ -1,12 +1,3 @@
-const todos = [
-    // {
-    //     title: 'make tea'
-    // },
-    // {
-    //     title: 'go to the grocery store'
-    // }
-];  // store todos here for now
-
 const createTodoWindow = {
     dialog: document.querySelector('.create-todo-dialog'),
     titleInput: document.querySelector('.create-todo-title'),
@@ -18,33 +9,6 @@ const createTodoWindow = {
 const backDrop = document.querySelector('.backdrop');
 const createTodoBtn = document.querySelector('.create-todo-button');
 const todoListItem = document.querySelector('.todo-list');
-
-// constructor function for a todo
-function Todo(title, body) {
-    this.id = uuid(),
-    this.title = title;
-    this.body = body;
-    this.completed = false;
-}
-
-// Saves this todo
-Todo.prototype.saveTodo = function() {
-    todos.push(this);  // TODO: make it asycnhronous and throw error if save is failed
-}
-
-Todo.prototype.deleteTodo = function() {
-    todos.splice(todos.indexOf(this), 1);  // TODO: Make this asynchronous and throw an error if failed
-}
-
-Todo.prototype.toggleTodo = function() {
-    this.completed = !this.completed;  // TODO: Make this asynchronous and throw an error if failed
-}
-
-// Asynchronous call which fetches Todos
-function getTodosAsync() {
-    // TODO: make asynchronous call and store the result in todos
-    return todos;
-}
 
 // returns a todo element
 function createTodoElement(todo) {
