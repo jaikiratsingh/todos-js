9de8bf02fd148c965cfc0e24eea9e957d71a225b
jaikiratsingh <sandhujaikirat98@gmail.com>
2020-09-07 16:28:40 +0530

    mvc restructure

---
+++ constants.js
@@ -0,0 +1,15 @@
+// priority options
+const priorities = {
+    NO : 'NO_PRIORITY',
+    LOW: 'LOW_PRIORITY',
+    MEDIUM: 'MEDIUM_PRIORITY',
+    HIGH: 'HIGH_PRIORITY'
+}
+
+// category options
+const categories = {
+    NO: 'default',
+    WORK: 'WORK',
+    HOBBY: 'HOBBY',
+    ADMIN: 'ADMIN',
+}
